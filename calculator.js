04c513b4e0dd86a36ce1ca6feb3fba57e6bde932
Dylan Lammert <Dylanjlammert@gmail.com>
2025-07-30 12:15:01 -0500

    Add functionality for the clear button. fixed some bugs with double operators in a single operation.

--- calculator.js
+++ calculator.js
@@ -24,27 +24,170 @@ function mult(num1, num2) {
 function div(num1, num2) {
     var dividend = 0;
     dividend = num1 / num2;
+    console.log(dividend);
     return dividend;
 };
 
 function operate(operator, num1, num2) {
+    let answer = 0;
     switch(operator){
         case "x":
             console.log("mult active");
-            mult(num1,num2);
+            answer = mult(num1,num2);
             break;
-        case "/":
+        case "÷":
             console.log("div active");
-            div(num1,num2);
+            answer = div(num1,num2);
             break;
         case "+":
-            add(num1,num2);
+            answer = add(num1,num2);
             break;
         case "-":
-            sub(num1,num2);
+            answer = sub(num1,num2);
             break;
         default:
             console.log("error");
     }
-    return 0;
+    return answer;
 };
+function isOperator(character) {
+    const operators = ['+', '-', 'x', '÷',' '];
+    return operators.includes(character);
+};
+
+function isPercentage(num) {
+    if(num[num.length -1] == '%') {
+        return true;
+    }
+    return false;
+}
+function percentToDecimal(numStr) {
+    let decConversion = 0;
+    let num = Number(numStr.slice(0,numStr.length - 1));
+    decConversion = num / 100;
+
+    return decConversion;
+}
+/**
+ * @brief helper function to find the index of an operator within a string
+ * 
+ */
+function findOperator(someString) {
+    let operatorIndex = 0;
+    for( let i = 0; i < someString.length; i++ ) {
+        if(isOperator(someString[i])) {
+            operatorIndex = i;
+            return operatorIndex;
+        }
+    }
+    return -1;
+}
+
+/**
+ * @brief This function checks the currentOperation string and determines if
+ *        the input that is coming in is a valid one. 
+ *        Cases(false return values): 
+ *        1. there are 2 operator next to each other
+ *        2. there are more than one decimal in a number
+ *        3. The percent sign follows an operand
+ *        4. there is not a close parenthesis to an open one
+ *        5. if there is already an operator in the string then it will act as the equateButton()
+ * @param {*} currentString This is the currentOperation string
+ * @param {*} input This is the input selection that is being verified
+ * @returns boolean value based on if the input is valid
+ */
+function verifyInput(currentString, input) {
+    console.log(currentString);
+    console.log(input);
+    if(isOperator(currentString[currentString.length - 1]) && isOperator(input)) {
+        console.log( errorText + "Error: Cannot have 2 operators together!" + resetColor)
+        return false;
+    }
+    for( let i = 0; i < currentString.length; i++) {
+        if(isOperator(currentString[i]) && isOperator(input)) {
+            console.log(warningTextBackground + blackText + "Completing operation on first operator" + resetColor);
+            currentOperation += "=";
+            equateButton();
+            //currentOperation += input;
+        }
+    }
+    return true;
+}
+
+function updateDisplay() {
+    displayArea.textContent = currentOperation;
+    prevAnswer.textContent = lastExpression + lastAnswer;
+}
+
+function equateButton() {
+    let num1 = 0;
+    let num2 = 0;
+    let num1Str = "";
+    let num2Str = "";
+    spliceIndex = findOperator(currentOperation);
+    console.log("CurrentOperation = " + currentOperation);
+    // need to check if the number is a percentage first
+    num1Str = currentOperation.slice(0,spliceIndex);
+    if(isPercentage(num1Str)) {
+        num1 = percentToDecimal(num1Str);
+    }else {
+        num1 = Number(num1Str)
+    }
+
+    let operator = currentOperation[spliceIndex];
+
+    // check to see if num2 is a percentage
+    num2Str = currentOperation.slice(spliceIndex + 1 , currentOperation.length - 1);
+    if(isPercentage(num2Str)) {
+        num2 = percentToDecimal(num2Str);
+    }else {
+        num2 = Number(num2Str)
+    }
+    // log the operation
+    console.log("operate" + "(" + operator + "," + num1 + "," + num2 + ")");
+    lastAnswer = operate(operator,num1,num2);
+    lastExpression = currentOperation;
+    currentOperation = lastAnswer;
+    updateDisplay();
+}
+
+// red error text color
+const errorText = "\x1b[41m";
+const resetColor = "\x1b[49m\x1b[39m";
+const warningTextBackground = "\x1b[43m";
+const blackText = "\x1b[30m";
+// grab the query selector for the display area
+const displayArea = document.querySelector("#displayArea");
+const prevAnswer = document.querySelector("#previousAnswer")
+// grab the query selector for the buttons
+const buttons = document.querySelectorAll(".calculatorButtons");
+
+// create a string to hold the current operation
+let currentOperation = "";
+
+let lastExpression = "";
+let lastAnswer = "";
+// add event listeners to each button so that when clicked, the button's value is added to the current operation string
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        let buttonValue = e.target.textContent;
+        if (verifyInput(currentOperation,buttonValue)) {
+            currentOperation += buttonValue;
+            displayArea.textContent = currentOperation;
+        };
+    });
+});
+
+// grab the reference to the clear button
+const clearButton = document.querySelector("#clearButton");
+
+clearButton.addEventListener("click", () => {
+    currentOperation = "";
+    displayArea.textContent = currentOperation;
+});
+
+// grab the reference to the equal sign button
+const equalsButton = document.querySelector("#equalOperation");
+
+// create an onclick event listener that 
+equalsButton.addEventListener("click", equateButton);
